f4fc5b54d97c1694a79199f12468810a410ce6cb
Kittipod Lambangchang <tonkittipod@gmail.com>
2022-06-21 01:00:02 +0700

    🎁 Update Files
    Project : 1,2,5

--- boilerplate-project-urlshortener-gomix/routes/urlRoute.js
+++
@@ -1,8 +0,0 @@
-const { getUrl, newUrl } = require("../controllers/urlController");
-const checkUrl = require("../middleware/validUrl");
-const router = require("express").Router();
-
-router.get("/:id", getUrl);
-router.post("/", checkUrl, newUrl);
-
-module.exports = router;
