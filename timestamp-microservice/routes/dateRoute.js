0febab6b7e9454936853a14fb5fa661355acd140
Kittonn <tonkittipod@gmail.com>
2023-12-14 15:36:17 +0700

    refactor: refactor code in timestamp microservice

--- timestamp-microservice/routes/dateRoute.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const { nowDate, singleDate } = require("../controllers/dateController");
-router.get("/", nowDate);
-router.get("/:date", singleDate);
-
-module.exports = router;
